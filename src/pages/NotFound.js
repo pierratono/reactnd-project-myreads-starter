5912dc8073a850f1364ebe7b2a02f543e19f151b
Pierratono Costa <pierratonoc@gmail.com>
2021-05-19 03:55:06 +0200

    finish-404 and clean app.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
     <div>
-      <h1>404</h1>
+      <figure className="not-found-gif">
+        <img
+          src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif"
+          alt="Page Not Found"
+        />
+      </figure>
+      <figcaption style={{ textAlign: "center" }}>
+        GIF by MINI Italia - https://gph.is/2EXDRe3
+      </figcaption>
+
+      <div className="home-link">
+        <Link to="/">Return to MyReads App</Link>
+      </div>
+      <p className="home-copyright">
+        Copyright © {new Date().getFullYear()} Pierratono Costa MAHORO
+      </p>
     </div>
   );
 }
